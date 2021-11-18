3ac7ce61acc0499900f59d3eb8f50037ada032a4
Muhammad Rafi Satria <89404089+Rapipipu@users.noreply.github.com>
2021-11-18 07:19:19 +0700

    Add files via upload

---
+++ Kolam Kayla.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int r;
+    float L;
+    
+    cin >> r;
+    
+    L = 3.14 * (r * r);
+    
+    printf("%.2f",L);
+    return 0;
+}
